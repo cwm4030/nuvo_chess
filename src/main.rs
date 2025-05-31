b6c47480458f052dc65fde170b7f1a623f3fe740
Caden Miller <cwmiller4030@proton.me>
2025-05-31 00:46:47 -0500

    small code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod board_state;
 mod universal_chess_interface;
 
-fn main() {
-    let mut board = board_state::board::Board::new();
+use board_state::board::Board;
+use universal_chess_interface::uci::uci_execute_command;
 
+fn main() {
+    let mut board = Board::new();
     let mut input = String::from("position startpos");
-    while universal_chess_interface::uci::uci_execute_command(&mut board, &input) {
+    while uci_execute_command(&mut board, &input) {
         input.clear();
         std::io::stdin().read_line(&mut input).expect("Failed to read line");
     }
